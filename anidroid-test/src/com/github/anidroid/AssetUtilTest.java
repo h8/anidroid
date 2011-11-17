e7a389db396442be6fb24db0504ff92dfdd0bfa9
Dmitry Stropaloff <helions8@gmail.com>
2011-11-17 20:26:08 +0200

    AssetUtil test case clean up

--- anidroid-test/src/com/github/anidroid/AssetUtilTest.java
+++ anidroid-test/src/com/github/anidroid/AssetUtilTest.java
@@ -24,57 +24,41 @@ import android.content.res.AssetManager;
 import android.test.InstrumentationTestCase;
 
 public class AssetUtilTest extends InstrumentationTestCase {
-	AssetManager assetManager;
+	AssetUtil util;
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
 
-		assetManager = getInstrumentation().getContext().getAssets();
+		final AssetManager assetManager = getInstrumentation()
+				.getContext().getAssets();
+		util = new AssetUtil(assetManager);
 	}
 
 	public void testGetFilesList_FrameFilesCount() {
-		final String assetPath = "ajax-loader";
-		final AssetUtil util = new AssetUtil(assetManager);
-
-		assertEquals(5, util.getFileList(assetPath).size());
+		assertEquals(5, util.getFileList("ajax-loader").size());
 	}
 
 	public void testGetFilesList_InvalidPath() {
-		final String assetPath = "not exists";
-		final AssetUtil util = new AssetUtil(assetManager);
-
-		assertEquals(0, util.getFileList(assetPath).size());
+		assertEquals(0, util.getFileList("not exists").size());
 	}
 
 	public void testGetFilesList_PathInFilename() {
-		final String assetPath = "ajax-loader";
-		final AssetUtil util = new AssetUtil(assetManager);
-
-		assertTrue(util.getFileList(assetPath).get(0).startsWith(assetPath));
+		assertTrue(util.getFileList("ajax-loader").get(0).startsWith("ajax-loader"));
 	}
 
 	public void testGetFilesList_FullFilename() {
-		final String assetPath = "ajax-loader";
-		final AssetUtil util = new AssetUtil(assetManager);
-
-		assertEquals(assetPath + "/frame_0.gif",
-				util.getFileList(assetPath).get(1));
+		assertEquals("ajax-loader" + "/frame_0.gif",
+				util.getFileList("ajax-loader").get(1));
 	}
 
 	public void testOpen_FileExists() {
-		final String fpath = "ajax-loader/ajax-loader.gif";
-		final AssetUtil util = new AssetUtil(assetManager);
-
-		assertNotNull(util.open(fpath));
+		assertNotNull(util.open("ajax-loader/ajax-loader.gif"));
 	}
 
 	public void testOpen_FileNotExists() {
-		final String fpath = "dir/some_file.txt";
-		final AssetUtil util = new AssetUtil(assetManager);
-
 		try {
-			util.open(fpath);
+			util.open("dir/some_file.txt");
 			fail();
 		} catch (final AniDroidException e) { }
 	}
